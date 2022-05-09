5aeb918997cc6b09f54a1be4effcddf9cdc4dde5
Claes Östlund <claes@stenbergit.se>
2022-05-09 18:14:05 +0200

    1.0.0

---
+++ PluginBootstrapList_v1.php
@@ -0,0 +1,33 @@
+<?php
+class PluginBootstrapList_v1{
+  public function widget_list($data){
+    /**
+     * 
+     */
+    wfPlugin::enable('icons/bootstrap_v1_8_1');
+    /**
+     * 
+     */
+    $data = new PluginWfArray($data['data']);
+    $element = new PluginWfYml(__DIR__.'/element/'.__FUNCTION__.'.yml');
+    $element->setByTag($data->get(), 'data');
+    $item = array();
+    foreach($data->get('item') as $v){
+      $i = new PluginWfArray($v);
+      $element_item = new PluginWfYml(__DIR__.'/element/'.__FUNCTION__.'_item.yml');
+      $element_item->setByTag($i->get(), 'rs', true);
+      /**
+       * 
+       */
+      if($i->get('target')){
+        $element_item->set('innerHTML/0/attribute/target', $i->get('target'));
+      }
+      /**
+       * 
+       */
+      $item[] = $element_item->get();
+    }
+    $element->setByTag(array('item' => $item));
+    wfDocument::renderElement($element);
+  }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# Buto-Plugin-BootstrapList_v1
+Render list with links.
+
+## Widget list
+```
+type: widget
+data:
+  plugin: bootstrap/list_v1
+  method: list
+  data:
+    title: 'Social media'
+    item: yml:/theme/[theme]/layout/navbar.yml:social_media
+```
+
+## Data
+```
+social_media:
+  -
+    href: 'https://www.facebook.com/MyPage'
+    icon: facebook
+    text: Facebook
+    target: _blank
+  -
+    href: 'https://twitter.com/MyPage'
+    icon: twitter
+    text: Twitter
+    target: _blank
+```
+
+### Icon
+Param icon (optional) is one of Bootstrap icons from plugin icons/bootstrap_v1_8_1.
